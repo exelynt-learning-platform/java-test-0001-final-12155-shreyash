7aceba455dfea9ca26270d1a70eea9c5c2a2f6b2
Shreyash14 <139038345+kawane123@users.noreply.github.com>
2026-03-14 10:56:44 +0530

    Add HollowDiamond class to print a hollow diamond shape

---
+++ HollowDiamond.java
@@ -0,0 +1,41 @@
+public class HollowDiamond {
+    public static void main(String[] args) {
+        int n = 5;
+
+        // upper half
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= 2 * i - 1; j++) {   // line 9
+                if (j == 1 || j == 2 * i - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+
+        // lower half
+        for (int i = n - 1; i >= 1; i--) {
+
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2 * i - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
